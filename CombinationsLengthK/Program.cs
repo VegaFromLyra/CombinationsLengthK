f7ba996233953f8b2253bb97fb035b5a73f1a5c9
VegaFromLyra <asha.ab@gmail.com>
2014-02-16 00:57:25 -0800

    Given an array of characters, return combinations of strings of length k

---
+++ CombinationsLengthK/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombinationsLengthK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 3;
+            string s = "ab";
+            Console.WriteLine("{0} combinations with string {1}", k, s);
+            PrintCombinations(s, k);
+        }
+
+        static void PrintCombinations(string s, int k)
+        {
+            string prefix = null;
+            PrintCombinations(s, k, prefix);
+        }
+
+        static void PrintCombinations(string s, int k, string prefix)
+        {
+            if (k == 0)
+            {
+                Console.WriteLine(prefix);
+            }
+            else
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    PrintCombinations(s, k - 1, prefix + s[i]);
+                }
+            }
+        }
+    }
+}
